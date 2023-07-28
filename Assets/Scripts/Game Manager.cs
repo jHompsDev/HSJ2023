33f2482f7ea715bfc9e33bb3a539c9a4318645a5
jHompsDev <jhompsgamedev@pm.me>
2023-07-28 13:26:08 +0100

    Created Scripts, GameManager

---
+++ Assets/Scripts/Game Manager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region VARIABLES
+    #endregion
+
+    #region FUNCTIONS
+    #endregion
+
+    #region MONOBEHAVIOUR
+    #endregion
+}
